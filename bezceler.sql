085bcdb3d8c639d781e458272f2b650e1eb79d86
DonnieDarko8881 <44477859+DonnieDarko8881@users.noreply.github.com>
2019-05-06 22:55:02 +0200

    poprawienie nazw zmiennych i procedur

--- bezceler.sql
+++ bezceler.sql
@@ -4,29 +4,29 @@ DELIMITER $$
 CREATE PROCEDURE UpdateBestsellers()
 BEGIN
     DECLARE numberOfLoans, days, bookId INT;
-    DECLARE LoansPerMonth DECIMAL(5,2);
+    DECLARE loansPerMonth DECIMAL(5,2);
     DECLARE finished INT default(0);
-    DECLARE ALL_BOOK CURSOR FOR SELECT BOOK_ID from BOOKS;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET FINISHED =1;
-    OPEN ALL_BOOK;
-    While (finished =0) DO
-		fetch ALL_BOOK INTO bookId;
+    DECLARE all_book CURSOR FOR SELECT BOOK_ID from BOOKS;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+    OPEN all_book;
+    WHILE (finished =0) DO
+		fetch all_book INTO bookId;
         if(finished = 0) THEN
 			SELECT COUNT(*) FROM RENTS WHERE BOOK_ID = bookId INTO numberOfLoans;
             SELECT DATEDIFF(CURDATE(), MIN(RENT_DATE)) + 1 from RENTS
 			where BOOK_ID = bookId into DAYS;
-            SET LoansPerMonth = numberOfLoans/days * 30;
-            if LoansPerMonth>2 THEN
+            SET loansPerMonth = numberOfLoans/days * 30;
+            IF loansPerMonth>2 THEN
 				UPDATE BOOKS SET BESTSELLER = true
 				WHERE BOOK_ID = bookId; 
 			else 
 				UPDATE BOOKS SET BESTSELLER = false
 				WHERE BOOK_ID = bookId; 
-			end if;
+			END IF;
 			COMMIT;
 		END IF;
 	END WHILE;
-    SELECT numberOfLoans, days, LoansPerMonth;
+    SELECT numberOfLoans, days, loansPerMonth;
 END $$
 DELIMITER ;
-call UpdateBestsellers();
+CALL UpdateBestsellers();
